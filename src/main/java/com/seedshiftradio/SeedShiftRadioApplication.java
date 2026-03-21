96b2c169d17356d68e5f2ac196687814a3999f40
iwaken0928@gmail.com <iwaken0928@gmail.com>
2026-03-21 16:49:15 +0900

    プロジェクトの初期構築実施

---
+++ src/main/java/com/seedshiftradio/SeedShiftRadioApplication.java
@@ -0,0 +1,13 @@
+package com.seedshiftradio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedShiftRadioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedShiftRadioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seedshiftradio/SeedShiftRadioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seedshiftradio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedShiftRadioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
